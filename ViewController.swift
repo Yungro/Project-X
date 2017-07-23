448c631dd77554b7f591593bef6a3a76e0f6f212
Yungro <idoering@cherrycreekschools.org>
2017-07-23 16:42:09 -0600

    Create ViewController.swift
    Make a single view app

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dream
+//
+//  This is a single view application
+//
+//  Created by Ian Doering on 7/23/17.
+//  Copyright © 2017 Ian Doering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
