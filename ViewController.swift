6a894dbe90ea291e112c275441acb8c4d8f24eb9
Yungro <idoering@cherrycreekschools.org>
2017-07-26 17:19:40 -0600

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cryptic
+//
+//  Created by Ian Doering on 7/25/17.
+//  Copyright © 2017 Ian Doering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
